b02e74b43d80459f6ac13b6b50c0c2746c8ad8cc
Michael Barrows <mail@michaelbarrows.com>
2022-04-16 17:07:25 +0100

    more sections

---
+++ src/components/sections/Contact.vue
@@ -0,0 +1,36 @@
+<template>
+    <section id="contact">
+        <div class="grid-container grid">
+            <div class="all-12">
+                <h2>Contact Me</h2>
+            </div>
+            <div class="all-12">
+                <div class="text semi-transparent-blue text-centre slight-rounding">
+                    <p>Got a project you'd like some help with or a vacancy you think I'd be a good fit for? Get in touch!</p>
+                    <p>If you'd rather email me, my email address is <a href="mailto:contact@michaelbarrows.com">contact@michaelbarrows.com</a>.</p>
+                </div>
+            </div>
+
+            <form action="#" method="post" id="contact-form" class="contact-form grid all-12">
+                <div class="small-12 medium-12 large-4 xlarge-4">
+                    <input type="text" name="name" placeholder="Name" required>
+                </div>
+                <div class="small-12 medium-12 large-4 xlarge-4">
+                    <input type="email" name="email_address" placeholder="E-Mail Address" required>
+                </div>
+                <div class="small-12 medium-12 large-4 xlarge-4">
+                    <input type="text" name="phone_number" placeholder="Phone Number" required>
+                </div>
+                <div class="small-12 medium-12 large-12 xlarge-12">
+                    <textarea name="message" placeholder="Your Message" class="message" rows="10" required></textarea>
+                </div>
+                <div class="small-12 medium-6 large-6 xlarge-6">
+                    <input type="reset" value="Reset Form">
+                </div>
+                <div class="small-12 medium-6 large-6 xlarge-6">
+                    <input type="submit" value="Send Message">
+                </div>
+            </form>
+        </div>
+    </section>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <script setup>
 import Landing from '@/components/sections/Landing.vue'
 import About from '@/components/sections/About.vue'
+import Contact from '@/components/sections/Contact.vue'
 </script>
 
 <template>
   <main>
     <Landing />
     <About />
+    <Contact />
   </main>
 </template>
