f9d0d72c1a1197bd006e0d65cd3d5bf4a2c22116
Michael Barrows <mail@michaelbarrows.com>
2022-04-17 16:27:49 +0100

    give it a name and sort out the indenting

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: () => import('../views/HomeView.vue')
-    },
-    {
-        path: '/project/:pretty_url',
-        component: () => import('../views/ProjectView.vue')
-    }
-  ]
-})
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: () => import('../views/HomeView.vue')
+        },
+        {
+            path: '/project/:pretty_url',
+            name: 'project',
+            component: () => import('../views/ProjectView.vue')
+        }
+    ]
+});
 
 export default router
