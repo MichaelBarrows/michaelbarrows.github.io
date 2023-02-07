8e65983410c91faba210ee326a8646ec5cd3553b
Michael Barrows <m+github@mbws.uk>
2023-02-07 19:42:21 +0000

    add computed duration

--- src/components/partials/EmploymentCard.vue
+++ src/components/partials/EmploymentCard.vue
@@ -9,6 +9,14 @@ import Card from './Card.vue';
 
         }
     },
+    computed: {
+        duration() {
+            const years = this.job.duration.years > 0 ? this.job.duration.years + ' Year ' : ''
+            const months = this.job.duration.months > 0 ? this.job.duration.months + ' Months' : ''
+
+            return years + months
+        }
+    },
     components: { Card }
 };
 </script>
@@ -16,7 +24,9 @@ import Card from './Card.vue';
 <template>
     <Card :title="job.title" :subTitle="job.company">
         <template v-slot:content>
-            <p class="align-center">{{ job.start_date }} - {{ job.end_date }}</p>
+            <p class="align-center">
+                {{ job.start_date }} - {{ job.end_date }}
+            </p>
             <span class="tag" v-for="tag in job.tags" :key="tag">{{ tag }}</span>
         </template>
         <template v-if="job.end_date == 'Present'" v-slot:flag>
