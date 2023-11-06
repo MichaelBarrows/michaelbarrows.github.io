2feba28a2303ee266936a7500a690d303599de7d
Michael Barrows <m+github@mbws.uk>
2023-11-06 17:24:14 +0000

    center modals vertically

--- components/BaseModal.vue
+++ components/BaseModal.vue
@@ -24,8 +24,8 @@ export default {
 </script>
 
 <template>
-  <div :class="(this.show ? '' : 'hidden ') + 'modal-container bg-slate-900 bg-opacity-70 overflow-y-auto h-full w-full z-50 fixed top-0 left-0'">
-    <div :class="(this.width || 'md:w-1/2') + ' modal mx-auto text-center bg-white rounded-lg w-11/12 m-5 ' + (this.position || 'relative')">
+  <div :class="(this.show ? '' : 'hidden ') + 'modal-container bg-slate-900 bg-opacity-70 overflow-y-auto h-full w-full z-50 fixed top-0 left-0 flex'">
+    <div :class="(this.width || 'md:w-1/2') + ' my-auto modal mx-auto text-center bg-white rounded-lg w-11/12 ' + (this.position || 'relative')">
       <div class="rounded-t-lg bg-gradient-to-br from-pacific-blue-600 to-pacific-blue-800 text-white shadow-md pb-3">
         <h2 class="modal-title text-2xl pt-3 text-xl md:text-2xl">{{ title }}</h2>
         <span class="absolute right-0 top-3">
