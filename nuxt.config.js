d64241391bc110e304194461548c907537b396e7
Michael Barrows <m+github@mbws.uk>
2023-11-19 17:24:18 +0000

    update title and favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Michael Barrows',
+    title: 'Michael Barrows | Software Engineer in Colchester, England',
     htmlAttrs: {
       lang: 'en'
     },
